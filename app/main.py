67e878a8cc227e81db9c821b3042d3bc80bc4cf3
Baptiste <102555318+NovemIgnotum@users.noreply.github.com>
2025-07-31 15:03:42 +0200

    Test de connection à la BDD ok

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-
+from sqlalchemy.exc import OperationalError
+from database.session import engine
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI with refresh"}
+    try:
+        with engine.connect() as connection:
+            return {"message": "Connexion à la base de données réussie !"}
+    except OperationalError as e:
+        return {"error": f"Erreur de connexion à la base de données : {e}"}
+
+
